ce7a18d332d9228480f3aae5f601c5a9b16fe68b
Vpiresgonca <vinicius@asimov.academy>
2024-03-08 13:29:21 -0300

    correções na vendas_por_canal

--- pages/Vendas_por_canal.py
+++ pages/Vendas_por_canal.py
@@ -14,7 +14,7 @@ try:
     sales_journeys = st.session_state['sales_journeys']
 except:
     sales_journeys = pd.read_feather(BytesIO(get_data_from_bucket(bucket_name='dashboard_marketing_processed', file_name='sales_journeys.feather', file_type='.feather')))
-    sales_journeys['approved_date'] = sales_journeys['approved_date'].apply(lambda x: pd.to_datetime(x[0]))
+    sales_journeys['order_date'] = sales_journeys['order_date'].apply(lambda x: x[0])
     st.session_state['sales_journeys'] = sales_journeys
 
 try:
@@ -57,7 +57,7 @@ def get_revenue_by_source_daily(user_journey_with_revenue: pd.DataFrame) -> pd.D
     user_journey = user_journey_with_revenue.copy()
     user_journey['revenue_per_source'] = user_journey['commission.value'] / user_journey['utm_source_std'].apply(len)
     
-    revenue_df = user_journey.explode('utm_source_std').groupby(['approved_date', 'utm_source_std'])['revenue_per_source'].sum().reset_index()
+    revenue_df = user_journey.explode('utm_source_std').groupby(['order_date', 'utm_source_std'])['revenue_per_source'].sum().reset_index()
     revenue_df['revenue_per_source'] = revenue_df['revenue_per_source'].astype(float)
     return revenue_df
 
@@ -78,15 +78,14 @@ name, authentication_status, username = authenticator.login('Login', 'main')
 if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'sidebar')
     st.title('Vendas por canal - Visão Geral')
-
     ##################### FILTERS ##########################################
 
     valid_hotmart = hotmart.loc[hotmart['status'].isin(['APPROVED','COMPLETE'])]
     today = datetime.today()
     date_range = st.sidebar.date_input("Periodo atual", value=(pd.to_datetime(valid_hotmart['order_date']).max()-timedelta(days=6), pd.to_datetime(valid_hotmart['order_date']).max()), max_value=pd.to_datetime(valid_hotmart['order_date']).max(), min_value=pd.to_datetime(valid_hotmart['order_date']).min())
 
-    limited_sales = sales_journeys.loc[(sales_journeys['approved_date'].dt.date >= date_range[0]) & (sales_journeys['approved_date'].dt.date <= date_range[1])].copy()
-    limited_hotmart = valid_hotmart.loc[(valid_hotmart['approved_date'].dt.date >= date_range[0]) & (valid_hotmart['approved_date'].dt.date <= date_range[1])]
+    limited_sales = sales_journeys.loc[(sales_journeys['order_date'].dt.date >= date_range[0]) & (sales_journeys['order_date'].dt.date <= date_range[1])].copy()
+    limited_hotmart = valid_hotmart.loc[(valid_hotmart['order_date'] >= date_range[0]) & (valid_hotmart['order_date'] <= date_range[1])]
     
 
     revenue_by_source = get_revenue_by_source(user_journey_with_revenue=limited_sales) 
@@ -114,12 +113,12 @@ if st.session_state["authentication_status"]:
         option = st.radio(label='Usar datas diferentes do período selecionado', options=['Sim', 'Não'], index=1)
         if option == 'Não':
             daily_revenue_by_source = get_revenue_by_source_daily(user_journey_with_revenue=limited_sales)
-            fig = px.line(data_frame=daily_revenue_by_source, x='approved_date', y='revenue_per_source', color='utm_source_std', title='Evolução do faturamento ao longo do tempo')
+            fig = px.line(data_frame=daily_revenue_by_source, x='order_date', y='revenue_per_source', color='utm_source_std', title='Evolução do faturamento ao longo do tempo')
         
         else:
-            new_dates = st.date_input("Selecione o periodo desejado", value=(sales_journeys['approved_date'].min(), sales_journeys['approved_date'].max()), max_value=sales_journeys['approved_date'].max(), min_value=sales_journeys['approved_date'].min())
-            tmp = sales_journeys.loc[(sales_journeys['approved_date'].dt.date >= new_dates[0]) & (sales_journeys['approved_date'].dt.date <= new_dates[1])]   
+            new_dates = st.date_input("Selecione o periodo desejado", value=(sales_journeys['order_date'].min(), sales_journeys['order_date'].max()), max_value=sales_journeys['approved_date'].max(), min_value=sales_journeys['approved_date'].min())
+            tmp = sales_journeys.loc[(sales_journeys['order_date']>= new_dates[0]) & (sales_journeys['order_date'] <= new_dates[1])]   
             daily_revenue_by_source = get_revenue_by_source_daily(tmp)            
-            fig = px.line(data_frame=daily_revenue_by_source, x='approved_date', y='revenue_per_source', color='utm_source_std', title='Evolução do faturamento ao longo do tempo')
+            fig = px.line(data_frame=daily_revenue_by_source, x='order_date', y='revenue_per_source', color='utm_source_std', title='Evolução do faturamento ao longo do tempo')
     
         st.plotly_chart(fig, use_container_width= True)
